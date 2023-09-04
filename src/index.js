e4c6769cb379ef03cd62ae2cd8bafd3e149ee5a6
kazamaki76@gmail.com <kazamaki76@gmail.com>
2023-09-04 20:10:15 +0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // For create UI 
+import ReactDOM from 'react-dom/client'; // for render 
+import './index.css'; // for styling 
+import { useState } from 'react';
 
+// form component// create fn
+// Task : Create HTML 
+// Task : ผูก State กับ UI 
+
+function Form() {
+
+  const [username, setUserName] = useState("");  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [cfPassword, setcfPassword] = useState("") 
+  
+  // Error state 
+  const [errorUserName, setErrorUserName] = useState("") ;
+  const [errorEmail, setErrorEmail] = useState("")
+  const [errorPassword, setErrorPassword] =useState("")
+  const [errorCfPassword, setErrorCfPassword] = useState ("")
+  
+  // color 
+  const [ usernameColor, setUserNamecolor] = useState("");
+  const [ emailColor, setEmailColor] = useState ("")
+  const [passwordColor, setPasswordColor ] = useState("")
+  const [ cfPasswordcolor, setCfPasswordColor] = useState("")
+
+  
+  const handleSubmit = (e) =>{ 
+    e.preventDefault();   // กันไม่ให้หน้า refresh 
+
+    // validate  username
+    if(username.length >= 8 ) {
+      setErrorUserName("")
+      setUserNamecolor("green")
+    } else {
+      setErrorUserName("กรุณาระบุชื่อผู้ใช้งานอย่างน้อย 8 ตัวอักษร")
+      setUserNamecolor("red")
+    }
+   
+    // validate email
+    if(email.includes("@")) {
+      setErrorEmail("")
+      setEmailColor("green")
+    } else {
+      setErrorEmail("wrong email")
+      setEmailColor("red")
+    }
+
+    if(password.length >=8 ) {
+      setErrorPassword("")
+      setPasswordColor("green")
+
+    } else { 
+      setErrorPassword("กรุณาระบุ Password อย่างน้อย 8 ตัวอักษร")
+      setPasswordColor("red")
+    }
+
+    if (password === cfPassword) {
+      setErrorCfPassword("")
+      setCfPasswordColor("green")
+    } else {
+      setErrorCfPassword("WRONG PASSWORD")
+      setCfPasswordColor("red")
+    }
+   
+
+
+    
+
+
+  };
+
+  return (
+    <div className='container'>
+      <form className='form'
+      onSubmit={handleSubmit}>
+        <div className='form-input' >
+          <label> Username         </label>
+          <input type='text' value={username} onChange={(e) => setUserName(e.target.value)}  style={{borderColor: usernameColor}}/> 
+          <small style={{color: usernameColor}} >{errorUserName} </small>    
+        </div>
+        {/* / Email  */}
+        <div className='form-input'> 
+        <label>Email </label>
+        <input type='email'value={email} onChange={(e) => setEmail(e.target.value)} style={{borderColor: emailColor}} />  
+        <small style={{color: emailColor}} > {errorEmail}  </small>
+        
+        </div>
+
+        {/* Password */}
+        <div className='form-input'> 
+        <label>Password </label>
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}  style={{borderColor :passwordColor }} />
+        <small style={{color: passwordColor}} >  {errorPassword} </small>
+        
+         </div>
+
+        {/* Confirm password */}
+
+
+        <div className='form-input'>
+
+        <label> Confirm-Password   </label>
+        <input type='password' value={cfPassword} onChange={(e) => setcfPassword(e.target.value)}  style={{borderColor : cfPasswordcolor}}/>
+        <small style={{color: cfPasswordcolor}} >{errorCfPassword} </small>
+        </div>
+        <button type='submit'> register </button>
+
+      </form>
+
+
+    </div>
+  )
+} 
+
+
+
+
+
+
+//  render
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(<Form/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
